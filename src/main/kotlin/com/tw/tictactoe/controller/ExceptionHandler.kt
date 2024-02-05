0039be9dad2593278c0b6be93ed07b74de51bafc
Mohd Qasim <qkqasimkhan02@gmail.com>
2024-02-05 21:47:09 +0530

    Add exception handler for illegal moves

---
+++ src/main/kotlin/com/tw/tictactoe/controller/ExceptionHandler.kt
@@ -0,0 +1,27 @@
+package com.tw.tictactoe.controller
+
+import com.tw.tictactoe.controller.dto.ErrorResponse
+import com.tw.tictactoe.exception.OutOfRangeException
+import com.tw.tictactoe.exception.PreOccupiedException
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@RestControllerAdvice
+class ExceptionHandler : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(PreOccupiedException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun preOccupiedErrorHandler(exception: PreOccupiedException): ErrorResponse {
+        println(exception.message)
+        return ErrorResponse(message = exception.message, position = exception.position)
+    }
+
+    @ExceptionHandler(OutOfRangeException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun outOfRangeErrorHandler(exception: OutOfRangeException): ErrorResponse {
+        println(exception.message)
+        return ErrorResponse(message = exception.message, position = exception.position)
+    }
+}
--- src/main/kotlin/com/tw/tictactoe/exception/OutOfRangeException.kt
+++ src/main/kotlin/com/tw/tictactoe/exception/OutOfRangeException.kt
@@ -1,6 +1,6 @@
 package com.tw.tictactoe.exception
 
 class OutOfRangeException(
-    private val position: Int,
+    val position: Int,
     override val message: String? = "$position is not in the range of 1 to 9"
 ) : Throwable()
